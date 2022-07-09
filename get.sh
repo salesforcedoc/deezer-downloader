54d24dbcefe4ac4f618cc44f724d901d3389dd8e
salesforcedocPTG <asim@plumtreegroup.net>
2022-07-08 22:32:35 -0400

    updates

---
+++ get.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python3 app/directdl.py \
+-i $1 \
+-u $2
+
--- start.sh
+++ start.sh
@@ -2,7 +2,9 @@
 
 # start the backend
 DEEZER_FLAC_QUALITY=
-python3 app/app.py  & 
+
+kill $(ps -ef | grep app.py | awk '{print $2}')
+python3 app/app.py & 
 # view frontend in the browser
 open http://localhost:5000
-ncmpcpp -h 127.0.0.1
+#ncmpcpp -h 127.0.0.1
